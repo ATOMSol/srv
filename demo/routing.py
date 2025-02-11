bfac04dd228c0decee609a138b07e05a6826d622
anurag <anurag@gmail.com>
2025-02-12 02:34:28 +0530

    check concetion

--- demo/routing.py
+++ demo/routing.py
@@ -5,7 +5,9 @@ from .consumers import appointment_consumers,call_consumer,canteen_consumer
 
 websocket_urlpatterns = [
     re_path(r'^ws/appointment/$', appointment_consumers.IndexPageConsumer.as_asgi()),
-    re_path(r'^ws/call/$', call_consumer.CallLiveConsumer.as_asgi()),
+    # re_path(r'^ws/call/$', call_consumer.CallLiveConsumer.as_asgi()),
+    re_path(r"^ws/call/(?P<user_id>[0-9a-fA-F-]+)/$",call_consumer.CallLiveConsumer.as_asgi()),
+
     re_path(r'^ws/snacks/$', canteen_consumer.OrderLiveConsumer.as_asgi()),
 
 ]
